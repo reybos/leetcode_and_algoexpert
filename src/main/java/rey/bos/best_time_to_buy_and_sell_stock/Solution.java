a023ed9ee56dfa8d714458080cd5f1c8ee02a9ce
reybos <andreybosiy@yandex.ru>
2024-06-08 11:36:09 +0200

    121. Best Time to Buy and Sell Stock

---
+++ src/main/java/rey/bos/best_time_to_buy_and_sell_stock/Solution.java
@@ -0,0 +1,18 @@
+package rey.bos.best_time_to_buy_and_sell_stock;
+
+public class Solution {
+
+    public int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            } else if (prices[i] - buy > profit) {
+                profit = prices[i] - buy;
+            }
+        }
+        return profit;
+    }
+    
+}
