5201e6304e4df98759071ebfefa3c79c932ffe59
reybos <andreybosiy@yandex.ru>
2024-06-13 11:19:55 +0200

    144. Binary Tree Preorder Traversal

---
+++ src/main/java/rey/bos/binary_tree_preorder_traversal/Solution.java
@@ -0,0 +1,41 @@
+package rey.bos.binary_tree_preorder_traversal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preorderTraversal(root, result);
+        return result;
+    }
+
+    private void preorderTraversal(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        result.add(root.val);
+        preorderTraversal(root.left, result);
+        preorderTraversal(root.right, result);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
