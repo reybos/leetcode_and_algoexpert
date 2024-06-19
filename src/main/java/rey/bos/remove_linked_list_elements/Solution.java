f5970f0ee4bf28a38c9bb470fe44f2da7c814ac1
reybos <andreybosiy@yandex.ru>
2024-06-19 10:37:54 +0200

    203. Remove Linked List Elements

---
+++ src/main/java/rey/bos/remove_linked_list_elements/Solution.java
@@ -0,0 +1,32 @@
+package rey.bos.remove_linked_list_elements;
+
+class Solution {
+    
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode result = null;
+        ListNode temp = null;
+        while (head != null) {
+            if (head.val != val) {
+                if (result == null) {
+                    result = new ListNode(head.val);
+                    temp = result;
+                } else {
+                    temp.next = new ListNode(head.val);
+                    temp = temp.next;
+                }
+            }
+            head = head.next;
+        }
+        return result;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+}
+
