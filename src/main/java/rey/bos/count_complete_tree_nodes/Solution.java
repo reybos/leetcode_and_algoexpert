33e9b32bbf6e694428746f45ad2814d4da10e194
reybos <andreybosiy@yandex.ru>
2024-06-26 12:55:29 +0200

    222. Count Complete Tree Nodes

---
+++ src/main/java/rey/bos/count_complete_tree_nodes/Solution.java
@@ -0,0 +1,34 @@
+package rey.bos.count_complete_tree_nodes;
+
+public class Solution {
+    
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
