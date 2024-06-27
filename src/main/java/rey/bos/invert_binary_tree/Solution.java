fae2c5fd325b13d3790fe774c21653b33f871288
reybos <andreybosiy@yandex.ru>
2024-06-27 10:22:43 +0200

    226. Invert Binary Tree

---
+++ src/main/java/rey/bos/invert_binary_tree/Solution.java
@@ -0,0 +1,36 @@
+package rey.bos.invert_binary_tree;
+
+public class Solution {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        return root;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
