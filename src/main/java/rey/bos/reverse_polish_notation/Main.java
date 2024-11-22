f03c76050da711a14e2d34a21b4d2d649c8ff389
reybos <andreybosiy@yandex.ru>
2024-11-22 16:03:55 -0600

    65. Reverse Polish Notation

--- src/main/java/rey/bos/reverse_polish_notation/Main.java
+++ src/main/java/rey/bos/reverse_polish_notation/Main.java
@@ -2,11 +2,12 @@ package rey.bos.reverse_polish_notation;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         String[][] arrays = new String[][] {
-            new String[] {"50", "3", "17", "+", "2", "-", "/"}
+            new String[] {"50", "3", "17", "+", "2", "-", "/"}, new String[] {"10"}, new String[] {"10", "5", "+"},
+            new String[] {"10", "5", "-"}, new String[] {"10", "5", "/"}, new String[] {"10", "5", "*"}
         };
-        int[] expected = new int[] {2};
+        int[] expected = new int[] {2, 10, 15, 5, 2, 50};
         for (int i = 0; i < expected.length; i++) {
             if (Solution.reversePolishNotation(arrays[i]) == expected[i]) {
                 System.out.println("test " + i + " passed");
--- src/main/java/rey/bos/reverse_polish_notation/Solution.java
+++ src/main/java/rey/bos/reverse_polish_notation/Solution.java
@@ -1,10 +1,30 @@
 package rey.bos.reverse_polish_notation;
 
+import java.util.List;
+import java.util.Stack;
+
 public class Solution {
 
-    public static int reversePolishNotation(String[] tokens) {
-        // Write your code here.
-        return -1;
+    public static int reversePolishNotation(String[] tokens) throws Exception {
+        Stack<Integer> storage = new Stack<>();
+        List<String> operations = List.of("/", "+", "-", "*");
+        for (String token : tokens) {
+            if (!operations.contains(token)) {
+                storage.push(Integer.parseInt(token));
+                continue;
+            }
+            int second = storage.pop();
+            int first = storage.pop();
+            int result = switch (token) {
+                case "/" -> first / second;
+                case "*" -> first * second;
+                case "+" -> first + second;
+                case "-" -> first - second;
+                default -> throw new Exception();
+            };
+            storage.push(result);
+        }
+        return storage.pop();
     }
 
 }
