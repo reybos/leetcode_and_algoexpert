e65d9f4e236060c1df9f7befc3a0329b1b5b8eb8
reybos <andreybosiy@yandex.ru>
2025-01-06 10:26:00 -0600

    09. Same BSTs

--- src/main/java/rey/bos/same_bsts/Main.java
+++ src/main/java/rey/bos/same_bsts/Main.java
@@ -1,19 +1,25 @@
 package rey.bos.same_bsts;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        int[][] arrays1 = new int[][] {
-            new int[] {10, 15, 8, 12, 94, 81, 5, 2, 11}, new int[] {1, 2, 3, 4, 5, 6, 7}, new int[] {7, 6, 5, 4, 3, 2, 1},
-            new int[] {1, 2, 3, 4, 5, 6, 7, 8}
-        };
-        int[][] arrays2 = new int[][] {
-            new int[] {10, 8, 5, 15, 2, 12, 11, 94, 81}, new int[] {1, 2, 3, 4, 5, 6, 7}, new int[] {7, 6, 5, 4, 3, 2, 1},
-            new int[] {1, 2, 3, 4, 5, 6, 7}
-        };
+        List<List<Integer>> array1 = List.of(
+            List.of(10, 15, 8, 12, 94, 81, 5, 2, 11),
+            List.of(1, 2, 3, 4, 5, 6, 7),
+            List.of(7, 6, 5, 4, 3, 2, 1),
+            List.of(1, 2, 3, 4, 5, 6, 7, 8)
+        );
+        List<List<Integer>> array2 = List.of(
+            List.of(10, 8, 5, 15, 2, 12, 11, 94, 81),
+            List.of(1, 2, 3, 4, 5, 6, 7),
+            List.of(7, 6, 5, 4, 3, 2, 1),
+            List.of(1, 2, 3, 4, 5, 6, 7)
+        );
         boolean[] expected = new boolean[] {true, true, true, false};
         for (int i = 0; i < expected.length; i++) {
-            if (Solution.sameBsts(arrays1[i], arrays2[i]) == expected[i]) {
+            if (Solution.sameBsts(array1.get(i), array2.get(i)) == expected[i]) {
                 System.out.println("test " + i + " passed");
             } else {
                 System.out.println("test " + i + " failed");
--- src/main/java/rey/bos/same_bsts/Solution.java
+++ src/main/java/rey/bos/same_bsts/Solution.java
@@ -1,10 +1,53 @@
 package rey.bos.same_bsts;
 
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
-    public static boolean sameBsts(int[] arrayOne, int[] arrayTwo) {
-        // Write your code here.
-        return false;
+    //10, 15, 8, 12, 94, 81, 5, 2, 11
+    //10, 8, 5, 15, 2, 12, 11, 94, 81
+    public static boolean sameBsts(List<Integer> arrayOne, List<Integer> arrayTwo) {
+        if (CollectionUtils.isEmpty(arrayOne) && CollectionUtils.isEmpty(arrayTwo)) {
+            return true;
+        } else if (arrayOne == null || arrayTwo == null) {
+            return false;
+        }
+        if (arrayOne.size() != arrayTwo.size() || arrayOne.get(0).intValue() != arrayTwo.get(0).intValue()) {
+            return false;
+        }
+        List<Integer> leftOne = getLeft(arrayOne);
+        List<Integer> leftTwo = getLeft(arrayTwo);
+        
+        List<Integer> rightOne = getRight(arrayOne);
+        List<Integer> rightTwo = getRight(arrayTwo);
+        return sameBsts(leftOne, leftTwo) && sameBsts(rightOne, rightTwo);
     }
-
+    
+    private static List<Integer> getLeft(List<Integer> array) {
+        int root = array.get(0);
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i < array.size(); i++) {
+            int num = array.get(i);
+            if (num < root) {
+                result.add(num);
+            }
+        }
+        return result;
+    }
+    
+    private static List<Integer> getRight(List<Integer> array) {
+        int root = array.get(0);
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i < array.size(); i++) {
+            int num = array.get(i);
+            if (num >= root) {
+                result.add(num);
+            }
+        }
+        return result;
+    }
+    
 }
