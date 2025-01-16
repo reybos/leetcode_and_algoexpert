b4796c5a3495f56be70a6fc0817b0da42911f2c8
reybos <andreybosiy@yandex.ru>
2025-01-16 15:27:30 -0600

    11. Repair BST

--- src/main/java/rey/bos/repair_bst/Solution.java
+++ src/main/java/rey/bos/repair_bst/Solution.java
@@ -12,9 +12,30 @@ public class Solution {
         }
     }
 
-    public static BST repairBst(BST tree) {
+    private static BST node1;
+    private static BST node2;
+    private static BST prevNode;
 
-        return null;
+    public static void repairBst(BST tree) {
+        findIncorrect(tree);
+        int temp = node1.value;
+        node1.value = node2.value;
+        node2.value = temp;
+    }
+    
+    private static void findIncorrect(BST tree) {
+        if (tree == null) {
+            return;
+        }
+        findIncorrect(tree.left);
+        if (prevNode != null && prevNode.value > tree.value) {
+            if (node1 == null) {
+                node1 = prevNode;
+            }
+            node2 = tree;
+        }
+        prevNode = tree;
+        findIncorrect(tree.right);
     }
 
 }
